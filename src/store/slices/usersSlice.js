5e5e297a2b34785d89d428af6e8b6e0147cafe48
jiyoungnim <cloundyon31@gmail.com>
2024-12-05 00:59:29 +0900

    ✨ 362. More on Adding Thunks ..Really HARD and FRUSTRATING TO UNDERSTAND AT ONCE!!

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // Update our state object however appropriate
+      // to show the user what we're loading data
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      // Assumption: action.payload = [{id: 1, name: 'Maya'}] = what we fetch from the API.
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      // when error comes up, action 객체 안에서 error라는 키 네임의 객체가 생성됨. (payload가 아님!)
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
+
+// extraReducers: allows us to watch for some very particular/additional action types that are being dispatched that are not inherently tied to the slice.
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// createAsyncThunk()의 매개변수 = 자동으로 디스패치되는 action 객체의 타입의 접두사로 들어갈 것 => 아무거나 해도 되지만, 나중에 디버깅 처리를 위해 이해될 만한 스트링으로 지정!
+// createAsyncThunk()의 매개변수 = 자동으로 디스패치되는 ✨action 객체의 타입 속성의 접두사✨로 들어갈 것 => 아무거나 해도 되지만, 나중에 디버깅 처리를 위해 이해될 만한 스트링으로 지정!
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("https://localhost:3005/users");
   // response.data = [{id: 1, name: 'myra'}]
