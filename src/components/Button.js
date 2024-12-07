a0355100bbd9a8756273c19193cc569286169e39
jiyoungnim <cloundyon31@gmail.com>
2024-12-08 01:17:57 +0900

    🚧 383. Creating A Fetch-Aware Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
-import className from 'classnames';
+import className from "classnames";
+import { GoSync } from "react-icons/go";
 
 function Button({
   children,
@@ -9,30 +10,34 @@ function Button({
   danger,
   outline,
   rounded,
+  loading,
   ...rest
 }) {
   const classes = className(
     rest.className,
-    'flex items-center px-3 py-1.5 border',
+    "flex items-center px-3 py-1.5 border h-8",
     {
-      'border-blue-500 bg-blue-500 text-white': primary,
-      'border-gray-900 bg-gray-900 text-white': secondary,
-      'border-green-500 bg-green-500 text-white': success,
-      'border-yellow-400 bg-yellow-400 text-white': warning,
-      'border-red-500 bg-red-500 text-white': danger,
-      'rounded-full': rounded,
-      'bg-white': outline,
-      'text-blue-500': outline && primary,
-      'text-gray-900': outline && secondary,
-      'text-green-500': outline && success,
-      'text-yellow-400': outline && warning,
-      'text-red-500': outline && danger,
+      "opacity-80": loading,
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
     }
   );
 
   return (
-    <button {...rest} className={classes}>
-      {children}
+    // 모든 HTML 버튼 요소들은 Disabled라는 Prop을 받는다.
+    // 해당 prop value가 true일 때마다, 버튼의 클릭 이벤트가 호출되지 않는다.(click event is blocked.)
+    <button {...rest} disabled={loading} className={classes}>
+      {loading ? <GoSync className="animate-spin" /> : children}
     </button>
   );
 }
@@ -48,7 +53,7 @@ Button.propTypes = {
 
     if (count > 1) {
       return new Error(
-        'Only one of primary, secondary, success, warning, danger can be true'
+        "Only one of primary, secondary, success, warning, danger can be true"
       );
     }
   },
