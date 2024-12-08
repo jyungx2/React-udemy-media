b3933582beefbcbb02a2c5a3744715e10c49d81f
jiyoungnim <cloundyon31@gmail.com>
2024-12-08 22:28:48 +0900

    🚧 390~393. Album Featrue Overview/Additional Components/Adding & Wrapping Up the ExpandablePanel

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,18 +11,29 @@ function UsersListItem({ user }) {
     doRemoveUser(user); // 삭제할 유저 데이터를 전달
   };
 
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user..</div>}
+      {user?.name}
+    </>
+  );
+
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user..</div>}
-          {user?.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>content</ExpandablePanel>
+    // <div className="mb-2 border rounded">
+    //   <div className="flex p-2 justify-between items-center cursor-pointer">
+    //     <div className="flex flex-row items-center justify-between">
+    //       <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+    //         <GoTrashcan />
+    //       </Button>
+    //       {error && <div>Error deleting user..</div>}
+    //       {user?.name}
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
